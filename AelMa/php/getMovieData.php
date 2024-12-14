50fbcd0538198974d86ee9299581ee75969e4275
sultan alif <163140590+sultanalif06@users.noreply.github.com>
2024-12-14 14:12:11 +0700

    Add files via upload

---
+++ AelMa/php/getMovieData.php
@@ -0,0 +1,30 @@
+<?php
+
+// Connecting to database AelMa
+require_once("config.php");
+
+if ($_POST) {
+    // Get input data
+	$movieID = filter_input(INPUT_POST, 'movie-id', FILTER_SANITIZE_STRING);
+
+	//Get movie data
+	// Preparing getMovieQuery
+	$getMovieQuery = "SELECT * FROM movies WHERE (movieID = :movieID)";
+	$stmt = $db->prepare($getMovieQuery);
+
+	// Bind getMovieQuery parameters
+	$params = array(
+		":movieID" => $movieID
+	);
+
+	// Execute getMovieQuery
+	$found = $stmt->execute($params);
+
+	if($found) {
+		$movieData = $stmt->fetch(PDO::FETCH_ASSOC);
+		echo json_encode($movieData, JSON_INVALID_UTF8_IGNORE);
+	} else {
+		$movieData = array("movieID" => "", "title" => "Movie Not Found", "rating" => "", "genre" => "", "duration" => "", "date" => "", "description" => "", "poster" => "");
+		echo json_encode($movieData, JSON_INVALID_UTF8_IGNORE);
+	}
+}
